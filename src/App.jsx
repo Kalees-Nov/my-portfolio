ea1874863ffb66f430660dacc9e6a7bb1722d62d
Kalees-Nov <kaleeswaran0711@gmail.com>
2025-08-15 23:53:45 +0530

    Initial portfolio commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // src/App.jsx
 import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar/Nav";
-import Home from "./components/Home/Home";
+import Home from "./Components/Home/Home";
 import AboutMe from "./Components/About/AboutMe";
-import Skills from "./components/Skills/Skill";
-import Projects from "./components/Project/Projects";
+import Projects from "./Components/Project/Projects";
+import Skills from "./Components/Skills/Skill";
 import Contact from "./Components/Contact/Contacts";
-import Footer from "./components/Footers/Footer";
+import Footer from "./Components/Footers/Footer";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./global.css";
